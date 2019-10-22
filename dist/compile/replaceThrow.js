b356fcd5f9a7ca08f628a56fc5bf8cea9fb87ab7
Mimiko Phonowell <phonowell@gmail.com>
2019-10-22 17:02:34 +0800

    updated to v0.0.11

--- dist/compile/replaceThrow.js
+++ dist/compile/replaceThrow.js
@@ -1,6 +1,27 @@
 (function() {
   module.exports = function(cont) {
-    return cont.replace(/throw/g, 'throw new Error').replace(/new Error new Error/g, 'new Error');
+    var i, j, len, line, listBoolean, listCont;
+    if (!cont) {
+      return '';
+    }
+    listBoolean = [cont.includes('throw'), cont.includes('reject')];
+    if (!listBoolean.includes(true)) {
+      return cont;
+    }
+    listCont = cont.split('\n');
+    for (i = j = 0, len = listCont.length; j < len; i = ++j) {
+      line = listCont[i];
+      if (line.trim()[0] === '#') {
+        continue;
+      }
+      listBoolean = [line.includes('throw '), line.includes('reject '), line.endsWith('throw'), line.endsWith('reject')];
+      if (!listBoolean.includes(true)) {
+        continue;
+      }
+      listCont[i] = line.replace(/throw\s/g, 'throw new Error ').replace(/reject\s/g, 'reject new Error ').replace(/new Error new Error/g, 'new Error');
+    }
+    // return
+    return listCont.join('\n');
   };
 
 }).call(this);
--- source/compile/replaceThrow.coffee
+++ source/compile/replaceThrow.coffee
@@ -1,5 +1,36 @@
 module.exports = (cont) ->
 
-  cont
-  .replace /throw/g, 'throw new Error'
-  .replace /new Error new Error/g, 'new Error'
+  unless cont
+    return ''
+
+  listBoolean = [
+    cont.includes 'throw'
+    cont.includes 'reject'
+  ]
+
+  unless listBoolean.includes true
+    return cont
+
+  listCont = cont.split '\n'
+  for line, i in listCont
+
+    if line.trim()[0] == '#'
+      continue
+
+    listBoolean = [
+      line.includes 'throw '
+      line.includes 'reject '
+      line.endsWith 'throw'
+      line.endsWith 'reject'
+    ]
+
+    unless listBoolean.includes true
+      continue
+
+    listCont[i] = line
+    .replace /throw\s/g, 'throw new Error '
+    .replace /reject\s/g, 'reject new Error '
+    .replace /new Error new Error/g, 'new Error'
+
+  # return
+  listCont.join '\n'
